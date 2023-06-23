26132b903ac7798688c548cf5a89b15751c1ea91
Nguyễn Tri Nhân <101175996+Nhan624@users.noreply.github.com>
2023-06-23 10:09:24 +0700

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,26 @@
+const Orders = [
+    {
+        productName: 'Jack',
+        productNumber: '15.000.000vnđ',
+        paymentStatus: '8.500.000vnđ',
+        paymentMoney: '23.000.000vnđ',
+        status: 'Đã nhận',
+        paymentLever: 'Không'
+    },
+    {
+        productName: 'Amir',
+        productNumber: '12.000.000vnđ',
+        paymentStatus: '5.200.000vnđ',
+        paymentMoney: '17.200.000vnđ',
+        status: 'Đã nhận',
+        paymentLever: 'Không'
+    },
+    {
+        productName: 'Ember',
+        productNumber: '18.000.000vnđ',
+        paymentStatus: '12.000.000vnđ',
+        paymentMoney: '30.000.000vnđ',
+        status: 'Đã nhận',
+        paymentLever: 'Nhân viên ưu tú'
+    },
+]
